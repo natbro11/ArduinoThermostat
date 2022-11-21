bb5342ab0f700be315dcacfe84345f51c7f64b42
Nathan Peroutka <natbro11@gmail.com>
2022-11-21 16:08:35 -0600

    better

--- Thermostat.ino
+++ Thermostat.ino
@@ -1,39 +1,36 @@
 
-//ToDo
-//1 - Temperature Smoothing?
-//2 - Better Date Logging?
-//3 - restruture, reduce sensor frequency
-//4
-//5
-
 #include <DHT.h>
-#include <DHT_U.h>
 #include <Adafruit_Sensor.h>
 #include <LiquidCrystal.h>
 
-DHT dht(2,DHT11);
-LiquidCrystal lcd(7,8,9,10,11,12);
+DHT dht(13,DHT11);
+LiquidCrystal lcd(8,9,4,5,6,7);
 
 byte f[] = {0x08,0x14,0x08,0x07,0x04,0x07,0x04,0x04}; //Degrees F character
-byte h[] = {0x12,0x04,0x08,0x12,0x00,0x05,0x07,0x05}; //Humidity character
+byte rh[] = {0x12,0x04,0x08,0x12,0x00,0x05,0x07,0x05}; //Humidity character
 byte l[] = {0x04,0x04,0x04,0x04,0x04,0x04,0x04,0x04}; //Divider character
 
-int cycleCount;        //Data recording - number of heat cycles since last restart
 int TempF;             //Temperature in Fahrenheit (Nearest Degree)
 int HumR;              //Relative Humidity (Nearest 5%)
-int fixT = 1;          //Temperature value offset for inaccurate sensor
+int fixT = 1.3;          //Temperature value offset for inaccurate sensor
 
-int setPoint = 72;     //Target Temperature
-int cycleTime = 90;    //How long the heat runs when called.
-int senseTime = 600;   //How long to wait between heat cycles.
+int setPoint = 73;     //Target Temperature
+int cycleTime = 60;    //How long the heat runs when called.
+int senseTime = 550;   //How long to wait between heat cycles.
+
+int cycleCount;        //Data recording - number of heat cycles since last restart
 
 void setup() {
-  pinMode(3, OUTPUT);
-  digitalWrite(3, LOW);   
+  delay(2000);
+  pinMode(12, OUTPUT);
+  digitalWrite(12, LOW);
+  pinMode(11, OUTPUT);
+  digitalWrite(11, HIGH);
+
   dht.begin();
   lcd.begin(16, 2);
   lcd.createChar(0, f);
-  lcd.createChar(1, h);
+  lcd.createChar(1, rh);
   lcd.createChar(2, l);
   lcdPrintOff();
 }
@@ -43,17 +40,37 @@ void loop() {
   if(TempF <= (setPoint - 2)){
     cycleHeat();
   }
+ int x;
+ x = analogRead (0);
+  if (x > 60 && x < 200) {
+    setPoint = 75;
+    if (setPoint > 75){
+      setPoint = 75;
+      lcd.print ("Go Fuck Yourself "); 
+    }
+  }
+  else if (x < 400){
+    setPoint = 68;
+  }
+  else if (x < 600){
+    setPoint = 73;
+  }
+  else if (x < 800){
+    cycleHeat();
+  }
   delay(5000);
 }
 
+
 /* Heating Cycle --------------------------------------------------------------------------------*/
 void cycleHeat(){
-  digitalWrite(3, HIGH); 
+  digitalWrite(12, HIGH); 
   for(int i = cycleTime; i > 0; i=i-5) {
     lcdPrintOn(i);
     delay(5000);
   }
-  digitalWrite(3, LOW); 
+  cycleCount = cycleCount + 1;
+  digitalWrite(12, LOW); 
   lcdPrintOff();
   senseHeat(senseTime);
 }
